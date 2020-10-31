d993db111e1373939b2090f8b630a4445a5e5f37
mtlcrusher <candrabriliantocakto@gmail.com>
2020-10-31 13:52:11 +0700

    add receiveFrame function

--- recvimg.cpp
+++ recvimg.cpp
@@ -7,6 +7,25 @@
 #define PORT 8080
 #define MAXLINE 256
 
+void receiveFrame(int *socketfd, sockaddr_in *address, uint *lenaddress, cv::Mat *frame, const size_t matSize, char *buffer)
+{
+  memset((char *)buffer, 0, matSize);
+  int n = recvfrom(*socketfd, (char *)buffer, 7, MSG_WAITALL, (sockaddr *)address, lenaddress);
+  if(memcmp((const char *)buffer, (const char *)"_START_", 7) == 0)
+  {
+    memset((char *)buffer, 0, matSize);
+    for(size_t i = 0; i < matSize; i += n)
+    {
+      n = recvfrom(*socketfd, (char *)buffer+i, matSize-i, MSG_WAITALL, (sockaddr *)address, lenaddress);
+      // if(n > 0)
+      // {
+      //   printf("numBytes = %d\n", n);
+      // }
+    }
+    memcpy((uchar *)frame->data, (char *)buffer, matSize);
+  }
+}
+
 int main(int argc, char **argv)
 {
   cv::Mat frame = cv::Mat(240, 320, CV_8UC3, cv::Scalar::all(0));
@@ -46,21 +65,7 @@ int main(int argc, char **argv)
   printf("ready!\n");
   while(1)
   {
-    memset((char *)buffer, 0, matSize);
-    n = recvfrom(sockfd, (char *)buffer, 7, MSG_WAITALL, (sockaddr *)&clientaddr, &lenclientaddr);
-    if(memcmp((const char *)buffer, (const char *)"_START_", 7) == 0)
-    {
-      memset((char *)buffer, 0, matSize);
-      for(size_t i = 0; i < matSize; i += n)
-      {
-        n = recvfrom(sockfd, (char *)buffer+i, matSize-i, MSG_WAITALL, (sockaddr *)&clientaddr, &lenclientaddr);
-        // if(n > 0)
-        // {
-        //   printf("numBytes = %d\n", n);
-        // }
-      }
-      memcpy((uchar *)frame.data, (char *)buffer, matSize);
-    }
+    receiveFrame(&sockfd, &clientaddr, &lenclientaddr, &frame, matSize, buffer);
     cv::imshow("received", frame);
     keyB = cv::waitKey(10);
     if(keyB == 'q')
